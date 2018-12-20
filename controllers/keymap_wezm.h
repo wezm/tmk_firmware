06ed96d9bfc4bc94510f6820210633c8f24d1d23
Wesley Moore <wes@wezm.net>
2018-12-20 11:49:27 +1100

    Make ` key do Escape on Filco

--- controllers/keymap_wezm.h
+++ controllers/keymap_wezm.h
@@ -1,8 +1,13 @@
+#include "action_util.h"
+
+// Used for SHIFT_ESC
+#define MODS_CTRL_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
 const uint8_t PROGMEM keymaps[][MATRIX_COLS][MATRIX_ROWS] = {
     /* Layer 0: Standard ANSI layer */
     KEYMAP(\
          ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12,   PSCR,SLCK,PAUS,                         \
-         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
+         FN9, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
          FN1, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,  PPLS, \
          FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,                      P4,  P5,  P6,        \
          LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT ,SLSH,     RSFT,        UP,           P1,  P2,  P3,  PENT, \
@@ -10,7 +15,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_COLS][MATRIX_ROWS] = {
     /* Layer 1: Function layer */
     KEYMAP(\
          TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,   TRNS,TRNS,TRNS,                         \
-         TRNS,TRNS,FN2 ,FN3 ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN7 ,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+         GRV ,TRNS,FN2 ,FN3 ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN7 ,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
          TRNS,TRNS,FN4 ,TRNS,TRNS,TRNS,HOME,PGDN,PGUP,END ,FN8 ,TRNS,TRNS,TRNS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LEFT,DOWN,UP  ,RGHT,TRNS,TRNS,     TRNS,                      TRNS,TRNS,TRNS,      \
          TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,FN5 ,TRNS,FN6 ,TRNS,     TRNS,        TRNS,         TRNS,TRNS,TRNS,TRNS, \
@@ -32,6 +37,13 @@ enum layer_id {
   LAYER_FUNCTION,
 };
 
+/*
+ * Fn action definition
+ */
+enum function_id {
+    SHIFT_ESC,
+};
+
 // Macros with personal details are put in here and not published to GitHub
 #include "keymap_wezm_private.h"
 
@@ -45,6 +57,7 @@ const action_t PROGMEM fn_actions[] = {
     [6] = ACTION_MACRO(MACRO_SIG),
     [7] = ACTION_MACRO(MACRO_YAY),
     [8] = ACTION_MACRO(MACRO_PHONE),
+    [9] = ACTION_FUNCTION(SHIFT_ESC),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -83,22 +96,30 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     return MACRO_NONE;
 }
 
-/*
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  (void)opt;
-  switch(id) {
-    case ACTION_LEDS_ALL:
-        if(record->event.pressed) {
-          // signal the LED controller thread
-          chMBPost(&led_mailbox, LED_MSG_GAME_TOGGLE, TIME_IMMEDIATE);
-        }
-      break;
-    case ACTION_LEDS_GAME:
-      if(record->event.pressed) {
-        // signal the LED controller thread
-        chMBPost(&led_mailbox, LED_MSG_ALL_TOGGLE, TIME_IMMEDIATE);
-      }
-      break;
-  }
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    static uint8_t shift_esc_shift_mask;
+    switch (id) {
+        case SHIFT_ESC:
+            shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
+            if (record->event.pressed) {
+                if (shift_esc_shift_mask) {
+                    add_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    add_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            } else {
+                if (shift_esc_shift_mask) {
+                    del_key(KC_GRV);
+                    send_keyboard_report();
+                } else {
+                    del_key(KC_ESC);
+                    send_keyboard_report();
+                }
+            }
+            break;
+    }
 }
-*/
